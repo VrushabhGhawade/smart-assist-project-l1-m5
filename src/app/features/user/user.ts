ac738c7b71db7563effeb5173322aa751d41adc3
AlfaStackNitoplus <alfastack.nitoplus@gmail.com>
2026-01-29 13:47:23 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/features/user/user.ts
+++ src/app/features/user/user.ts
@@ -3,17 +3,29 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
 import { MockData } from '../../assets/mock-data';
+import { Ticket } from '../../core/models/ticket.model';
+import { MatTab } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
 
 
 @Component({
   selector: 'app-user',
-  imports: [MatToolbarModule, MatCardModule],
+  imports: [MatToolbarModule, MatCardModule,MatTableModule],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
 export class User {
   userName: string = '';
   userId: string = '';
+  userTickets: Ticket[]= [];
+  displayedColumns: string[] = [
+    'ticketId',
+    'description',
+    'priority',
+    'status',
+    'age',
+    'assignee'
+  ];
   constructor(
     private route: ActivatedRoute
   ) {
@@ -21,5 +33,12 @@ export class User {
       this.userId = params['id'];
     });
     this.userName = MockData.users.find(u => u.userId === this.userId)?.name || '';
+     this.userTickets = MockData.tickets.filter(
+      t => t.createdByUserId === this.userId
+    );
+  }
+  getAssigneeName(assigneeId?: string): string {
+    if (!assigneeId) return '-';
+    return MockData.users.find(u => u.userId === assigneeId)?.name ?? '-';
   }
 }
